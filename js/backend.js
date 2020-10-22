a1f025ae00f4268780472d51e15e16f829037e75
EfimovKirill <heykirill@gmail.com>
2020-10-22 23:54:36 +0300

    Выполнение задачи 7.1

--- js/backend.js
+++ js/backend.js
@@ -25,7 +25,7 @@
     });
 
     xhr.addEventListener(`timeout`, () => {
-        onError(`Запрос не успел выполниться за ` + xhr.timeout + `мс`);
+      onError(`Запрос не успел выполниться за ` + xhr.timeout + `мс`);
     });
 
     xhr.timeout = TIMEOUT_IN_MS;
